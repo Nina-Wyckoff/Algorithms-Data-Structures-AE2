f412a22fd0290dabb84d9dd1cd9382a2f261666a
“Nina <2917069w@student.gla.ac.uk>
2025-03-22 16:00:28 +0000

    Created Files and wrote Dynamic Set Interface

---
+++ java/DynamicSetADT.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+
+
+
+public interface DynamicSetADT {
+
+
+    //add element x to S, if it is not present already
+    public void add(HashSet<Object> setS, Object elementX);
+
+    //remove element x from S, if it is presen
+    public void remove(HashSet<Object> setS, Object elementX);
+
+    //check whether element x is in set S
+    public boolean isElement(HashSet<Object> setS, Object elementX);
+
+    //check whether set S has no elements
+    public boolean isEmpty(HashSet<Object> setS);
+
+    //return the number of elements of set S
+    public int setSize(HashSet<Object> setS);
+
+    // return the union of sets S and T
+    public HashSet<Object> union(HashSet<Object> setS, HashSet<Object> setT);
+
+    //return the intersection of sets S and T
+    public HashSet<Object> intersection(HashSet<Object> setS, HashSet<Object> setT);
+
+    //returns the difference of sets S and T
+    public HashSet<Object> differences(HashSet<Object> setS, HashSet<Object> setT);
+
+    //check whether set S is a subset of set T
+    public boolean subset(HashSet<Object> setS, HashSet<Object> setT);
+}
