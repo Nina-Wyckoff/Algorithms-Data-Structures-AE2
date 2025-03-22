a006a53ef6b21757ab39682488f9f3b8d1c85494
“Nina <2917069w@student.gla.ac.uk>
2025-03-22 18:01:53 +0000

    Introduced Node class

--- src/java/BinarySearchTreeOnADT/BinSearchTreeDynSet.java
+++ src/java/BinarySearchTreeOnADT/BinSearchTreeDynSet.java
@@ -1,55 +1,80 @@
 package BinarySearchTreeOnADT;
 
 import java.util.HashSet;
+import java.util.NoSuchElementException;
 
 //implementation of DynSet Interface
 //define BST Structure
 //Node class?
 
-class BinarySearchTreeOnADT implements DynamicSetADT{
+public class BinSearchTreeDynSet implements DynamicSetADT<BinarySearchTreeOnADT.BinSearchTreeDynSet.Node>{
+
+    private Node root; //root of Binary Search Tree
+
+        protected class Node{
+        private int key; //sort by key
+        private Node leftNode, rightNode, p; //three slots
+        private int size; //number of nodes
+
+        public Node(int key, int size){
+            this.key = key;
+            this.size = size;
+            this.leftNode = null;
+            this.rightNode = null;
+            this.p = null;
+        }
+    }
+   
+    public BinSearchTreeDynSet(){
+        root = null;
+
+    }
+    
+    
 
     //add element x to S, if it is not present already
-    public void add(HashSet<Object> setS, Object elementX){
+    public void add(HashSet<Node> setS, Node elementX){
 
     }
 
     //remove element x from S, if it is presen
-    public void remove(HashSet<Object> setS, Object elementX){
+    public void remove(HashSet<Node> setS, Node elementX){
         
     }
 
     //check whether element x is in set S
-    public boolean isElement(HashSet<Object> setS, Object elementX){
+    
+    public boolean isElement(HashSet<Node> setS, Node elementX){
         
     }
 
     //check whether set S has no elements
-    public boolean isEmpty(HashSet<Object> setS){
+    public boolean isEmpty(HashSet<Node> setS){
         
     }
 
     //return the number of elements of set S
-    public int setSize(HashSet<Object> setS){
+    public int setSize(HashSet<Node> setS){
         
     }
 
     // return the union of sets S and T
-    public HashSet<Object> union(HashSet<Object> setS, HashSet<Object> setT){
+    public HashSet<Node> union(HashSet<Node> setS, HashSet<Node> setT){
         
     }
 
     //return the intersection of sets S and T
-    public HashSet<Object> intersection(HashSet<Object> setS, HashSet<Object> setT){
+    public HashSet<Node> intersection(HashSet<Node> setS, HashSet<Node> setT){
         
     }
 
     //returns the difference of sets S and T
-    public HashSet<Object> differences(HashSet<Object> setS, HashSet<Object> setT){
+    public HashSet<Node> differences(HashSet<Node> setS, HashSet<Node> setT){
         
     }
 
     //check whether set S is a subset of set T
-    public boolean subset(HashSet<Object> setS, HashSet<Object> setT){
+    public boolean subset(HashSet<Node> setS, HashSet<Node> setT){
         
     }
 
--- src/java/BinarySearchTreeOnADT/DynamicSetADT.java
+++ src/java/BinarySearchTreeOnADT/DynamicSetADT.java
@@ -3,33 +3,33 @@ package BinarySearchTreeOnADT;
 
 import java.util.HashSet;
 
-public interface DynamicSetADT {
+public interface DynamicSetADT<T> {
 
 
     //add element x to S, if it is not present already
-    public void add(HashSet<Object> setS, Object elementX);
+    public void add(HashSet<T> setS, T elementX);
 
     //remove element x from S, if it is presen
-    public void remove(HashSet<Object> setS, Object elementX);
+    public void remove(HashSet<T> setS, T elementX);
 
     //check whether element x is in set S
-    public boolean isElement(HashSet<Object> setS, Object elementX);
+    public boolean isElement(HashSet<T> setS, T elementX);
 
     //check whether set S has no elements
-    public boolean isEmpty(HashSet<Object> setS);
+    public boolean isEmpty(HashSet<T> setS);
 
     //return the number of elements of set S
-    public int setSize(HashSet<Object> setS);
+    public int setSize(HashSet<T> setS);
 
     // return the union of sets S and T
-    public HashSet<Object> union(HashSet<Object> setS, HashSet<Object> setT);
+    public HashSet<T> union(HashSet<T> setS, HashSet<T> setT);
 
     //return the intersection of sets S and T
-    public HashSet<Object> intersection(HashSet<Object> setS, HashSet<Object> setT);
+    public HashSet<T> intersection(HashSet<T> setS, HashSet<T> setT);
 
     //returns the difference of sets S and T
-    public HashSet<Object> differences(HashSet<Object> setS, HashSet<Object> setT);
+    public HashSet<T> differences(HashSet<T> setS, HashSet<T> setT);
 
     //check whether set S is a subset of set T
-    public boolean subset(HashSet<Object> setS, HashSet<Object> setT);
+    public boolean subset(HashSet<T> setS, HashSet<T> setT);
 }
